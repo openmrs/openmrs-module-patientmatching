50d28ea1b4fea0a2e0cb2406d2533cc556f852d3
James Egg <jegg@regenstrief.org>
2008-06-09 13:21:43 +0000

    in patientmatching module, added a second method to transitive similarity class and wrote javadoc for all methods

--- src/org/regenstrief/linkage/analysis/SetSimilarityAnalysis.java
+++ src/org/regenstrief/linkage/analysis/SetSimilarityAnalysis.java
@@ -1,6 +1,7 @@
 package org.regenstrief.linkage.analysis;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.regenstrief.linkage.MatchResult;
@@ -26,10 +27,42 @@ import org.regenstrief.linkage.MatchResult;
 
 public class SetSimilarityAnalysis {
 	
+	/**
+	 * Method returns the argument list separated out into sub lists
+	 * based on similarity between both Records in the MatchResult objects
+	 * 
+	 * @param results	a single list of all the MatchResult objects
+	 * @return	a sublists of results, where all MatchResult objects in a sublist have Records in common
+	 */
 	public List<List<MatchResult>> getSimilarSets(List<MatchResult> results){
+		/*
+		 * Method not currently implemented, just copies results to ret
+		 */
 		
 		List<List<MatchResult>> ret = new ArrayList<List<MatchResult>>();
 		ret.add(results);
 		return ret;
+	}
+	
+	/**
+	 * Method performs the same grouping as getSimilarSets(List<MatchResult> results), but
+	 * it takes a threshold that MatchResult objects must meet before being inserted into
+	 * the return object
+	 * 
+	 * @param results	a single list of all the MatchResult objects
+	 * @param threshold	a score requirement for a MatchResult to be copied into the return object
+	 * @return	a sublists of results, where all MatchResult objects in a sublist have Records in common and meet the threshold
+	 */
+	public List<List<MatchResult>> getSimilarSets(List<MatchResult> results, double threshold){
+		List<MatchResult> true_matches = new ArrayList<MatchResult>();
+		Iterator<MatchResult> it = results.iterator();
+		while(it.hasNext()){
+			MatchResult mr = it.next();
+			if(mr.getScore() > threshold){
+				true_matches.add(mr);
+			}
+		}
+		
+		return getSimilarSets(true_matches);
 	}
 }
