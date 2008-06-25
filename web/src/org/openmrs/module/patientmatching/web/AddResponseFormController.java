cda598f57abc444df24728f6de83b0beeacc4bb5
Nyoman Ribeka <nyoman@openmrs.org>
2008-06-25 21:26:12 +0000

    patient matching module: add some web pages to the module. need to decide on how to move on with the web section of the module.

---
+++ web/src/org/openmrs/module/patientmatching/web/AddResponseFormController.java
@@ -0,0 +1,39 @@
+package org.openmrs.module.patientmatching.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+public class AddResponseFormController extends SimpleFormController {
+	
+    /** Logger for this class and subclasses */
+    protected final Log log = LogFactory.getLog(getClass());
+
+	@Override
+	protected String formBackingObject(HttpServletRequest request) throws Exception {
+		
+		//String text = Context.getService(HelloWorldService.class);
+		
+		String text = "Not used";
+		
+		log.debug("Returning text: " + text);
+		
+		return text;
+		
+	}
+
+	@Override
+	protected Map<String, Object> referenceData(HttpServletRequest req) throws Exception {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		return map;
+		
+	}
+	
+}
