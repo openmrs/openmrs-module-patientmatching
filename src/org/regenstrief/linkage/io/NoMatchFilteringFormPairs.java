b8213374d1a2ec94daa7a61c86979e42a4fa2e1a
James Egg <jegg@regenstrief.org>
2011-11-02 18:07:34 +0000

    in patientmatching module, added a new FormPairs that takes another FormPairs, and only allows pairs through that agree on fields other than blocking and sex/gender.  Not incorporated into GUI yet

---
+++ src/org/regenstrief/linkage/io/NoMatchFilteringFormPairs.java
@@ -0,0 +1,72 @@
+package org.regenstrief.linkage.io;
+
+import java.util.Iterator;
+
+import org.regenstrief.linkage.MatchResult;
+import org.regenstrief.linkage.Record;
+import org.regenstrief.linkage.util.ScorePair;
+
+/**
+ * Class takes a FormPairs object, and only passes through pairs which match
+ * on one of the include fields other than sex/gender
+ * 
+ * @author jegg
+ *
+ */
+
+public class NoMatchFilteringFormPairs extends FormPairs {
+
+	private FormPairs fp;
+	private ScorePair sp;
+	private String allowed_demographic;
+	
+	public NoMatchFilteringFormPairs(FormPairs fp){
+		super(fp.getMatchingConfig());
+		this.fp = fp;
+		sp = new ScorePair(fp.getMatchingConfig());
+		allowed_demographic = "gender";
+	}
+	
+	public String getAllowedDemographic(){
+		return allowed_demographic;
+	}
+	
+	public void setAllowedDemographic(String demographic){
+		allowed_demographic = demographic;
+	}
+	
+	@Override
+	public Record[] getNextRecordPair() {
+		boolean finished = false;
+		Record[] candidate = null;
+		while(!finished){
+			candidate = fp.getNextRecordPair();
+			if(candidate == null){
+				finished = true;
+			} else {
+				if(passesFilter(candidate)){
+					finished = true;
+				}
+			}
+		}
+		
+		return candidate;
+	}
+	
+	private boolean passesFilter(Record[] pair){
+		MatchResult mr = sp.scorePair(pair[0], pair[1]);
+		boolean passes = false;
+		Iterator<String> it = mr.getMatchVector().getDemographics().iterator();
+		while(it.hasNext() && !passes){
+			String demographic = it.next();
+			if(!demographic.equals(allowed_demographic)){
+				if(mr.getMatchVector().matchedOn(demographic)){
+					passes = true;
+				}
+			}
+		}
+		
+		return passes;
+	}
+
+}
