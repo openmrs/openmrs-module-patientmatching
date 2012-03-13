d6dbbc5e0d789e556fea3e3d04a4490e2fa24325
Jeremy Keiper <jeremy@openmrs.org>
2012-03-13 19:08:10 +0000

    Patient Matching Module - adding GUI project

---
+++ gui/src/main/java/org/openmrs/module/patientmatching/gui/App.java
@@ -0,0 +1,13 @@
+package org.openmrs.module.patientmatching.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
