236c551c666dda8e87534afd85018dda9231490f
Jeremy Keiper <jeremy@openmrs.org>
2012-03-14 14:04:16 +0000

    Patient Matching Module - removed unused reference to DWR class from XMLUpdater

--- api/src/main/java/org/regenstrief/linkage/util/XMLUpdater.java
+++ api/src/main/java/org/regenstrief/linkage/util/XMLUpdater.java
@@ -19,7 +19,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.openmrs.module.patientmatching.web.dwr.DWRMatchingConfigUtilities;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
