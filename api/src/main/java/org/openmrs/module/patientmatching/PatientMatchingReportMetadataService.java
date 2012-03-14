5f221fea479b3fe2626b680f17f7f002f5bf4f5b
Jeremy Keiper <jeremy@openmrs.org>
2012-03-14 14:23:58 +0000

    Patient Matching Module - moved method from service to controller

--- api/src/main/java/org/openmrs/module/patientmatching/PatientMatchingReportMetadataService.java
+++ api/src/main/java/org/openmrs/module/patientmatching/PatientMatchingReportMetadataService.java
@@ -1,14 +1,18 @@
 package org.openmrs.module.patientmatching;
+
 import java.util.List;
-import java.util.Map;
-import org.openmrs.module.patientmatching.PatientMatchingReportMetadata;
+
 public interface PatientMatchingReportMetadataService {
-	
-    public void saveReportDetails(PatientMatchingReportMetadata pri);
-    public Map<String, Object> showReportDetails(String reportName);
-    public void savePatientMatchingConfiguration(PatientMatchingConfiguration patientMatchingConfiguration);
-    public void deletePatientMatchingConfigurationByName(String name);
-    public PatientMatchingConfiguration findPatientMatchingConfigurationByName(String name);
-    public List<PatientMatchingConfiguration> getBlockingRuns();
-    public List<PatientMatchingConfiguration> getMatchingConfigs();
+
+	public void saveReportDetails(PatientMatchingReportMetadata pri);
+
+	public void savePatientMatchingConfiguration(PatientMatchingConfiguration patientMatchingConfiguration);
+
+	public void deletePatientMatchingConfigurationByName(String name);
+
+	public PatientMatchingConfiguration findPatientMatchingConfigurationByName(String name);
+
+	public List<PatientMatchingConfiguration> getBlockingRuns();
+
+	public List<PatientMatchingConfiguration> getMatchingConfigs();
 }
