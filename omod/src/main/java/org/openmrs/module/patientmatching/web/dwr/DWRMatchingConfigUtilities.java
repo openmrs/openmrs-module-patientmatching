6ee57f19ef62eea642b18d1642c938cd2033f8ab
Lahiru Jayathilake <lahirujayathilake@gmail.com>
2017-08-12 19:34:56 +0530

    PTM-87 : Functionality to Merge Patients in the report

--- omod/src/main/java/org/openmrs/module/patientmatching/web/dwr/DWRMatchingConfigUtilities.java
+++ omod/src/main/java/org/openmrs/module/patientmatching/web/dwr/DWRMatchingConfigUtilities.java
@@ -34,6 +34,7 @@ import org.openmrs.module.patientmatching.MatchingRunData;
 import org.openmrs.module.patientmatching.PatientMatchingConfiguration;
 import org.openmrs.module.patientmatching.PatientMatchingReportMetadataService;
 import org.openmrs.module.patientmatching.Report;
+import org.openmrs.serialization.SerializationException;
 import org.regenstrief.linkage.util.MatchingConfig;
 
 import uk.ltd.getahead.dwr.WebContext;
@@ -58,6 +59,7 @@ public class DWRMatchingConfigUtilities {
 	private static int currentStep;
 	private static int size = 0;
 	private static String[] selectedStrat;
+	private static boolean isIncrementalMatch;
 
 	/**
 	 * Constructor
@@ -169,6 +171,7 @@ public class DWRMatchingConfigUtilities {
 			switch (nextStep) {
 				case 2:
 					objects = new HashMap<String, Object>();
+					objects.put("isIncremental",isIncrementalMatch);
 					objects = MatchingReportUtils.ReadConfigFile(objects, selectedStrat);
 					break;
 
@@ -219,8 +222,9 @@ public class DWRMatchingConfigUtilities {
 		time = Calendar.getInstance().getTimeInMillis() - time;
 	}
 
-	public void doAnalysis(String selectedStrategies) {
+	public void doAnalysis(String selectedStrategies, boolean isIncremental) {
 		MatchingRunData.getInstance().setFileStrat(selectedStrategies);
+		isIncrementalMatch = isIncremental;
 		if (!MatchingRunData.getInstance().isTimerTaskStarted()) {
 			selectedStrat = selectedStrategies.split(",");
 			MatchingRunData.getInstance().setProTimeList(new ArrayList<Long>());
@@ -422,5 +426,33 @@ public class DWRMatchingConfigUtilities {
 
         currentContent = reader.fetchContent(currentPage);
         return currentContent;
+	}
+
+	/**
+	 *  Merge patients
+	 *
+	 * @param listPatientIDs list of patient ids separated by & character
+	 * @return true if merge successful or false otherwise
+	 */
+	public static boolean mergePatients(String listPatientIDs) {
+		String [] strPatientIDs = listPatientIDs.split("&");
+		// There should be at least two patients
+		if (strPatientIDs.length <= 1) {
+			return false;
+		}
+
+		try {
+			List<Patient> patients = new ArrayList<Patient>();
+			for (String id : strPatientIDs) {
+				Patient p = Context.getPatientService().getPatient(Integer.valueOf(id));
+			    patients.add(p);
+			}
+		    // merge patients
+			Context.getPatientService().mergePatients(patients.remove(0), patients);
+			return true;
+
+		} catch (SerializationException e) {
+			return false;
+		}
 	}
 }
