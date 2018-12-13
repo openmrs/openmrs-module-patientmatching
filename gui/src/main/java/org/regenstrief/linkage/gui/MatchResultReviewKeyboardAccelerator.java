47213583c0b6f54289bc1ca6b2eede01a3539eb5
Andrew Martin <am202@regenstrief.org>
2018-12-13 10:30:34 -0500

    Fix manual review software bugs; add utility code

--- gui/src/main/java/org/regenstrief/linkage/gui/MatchResultReviewKeyboardAccelerator.java
+++ gui/src/main/java/org/regenstrief/linkage/gui/MatchResultReviewKeyboardAccelerator.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import javax.swing.JTextField;
 
-public class MatchResultReviewKeyboardAccelerator implements KeyListener, ContainerListener{
+public class MatchResultReviewKeyboardAccelerator implements KeyListener, ContainerListener {
 
 	public static final MatchResultReviewKeyboardAccelerator INSTANCE = new MatchResultReviewKeyboardAccelerator();
 	
@@ -18,53 +18,47 @@ public class MatchResultReviewKeyboardAccelerator implements KeyListener, Contai
 	private MatchResultReviewPagerPanel pager;
 	private int focus_index;
 	
-	private MatchResultReviewKeyboardAccelerator(){
+	private MatchResultReviewKeyboardAccelerator() {
 		focus_index = 0;
 	}
 	
-	public void setReviewPanelList(List<MatchResultReviewPanel> panels){
+	public void setReviewPanelList(final List<MatchResultReviewPanel> panels) {
 		review_panels = panels;
 	}
 	
-	public void setPagerPanel(MatchResultReviewPagerPanel mrppp){
+	public void setPagerPanel(final MatchResultReviewPagerPanel mrppp) {
 		pager = mrppp;
 	}
 
-	public void componentAdded(ContainerEvent arg0) {
+	@Override
+	public void componentAdded(final ContainerEvent arg0) {
 		addAccelerator(arg0.getChild());
 	}
 	
-	private void addAccelerator(Component comp){
-		//System.out.println("added key listener to " + comp);
+	private void addAccelerator(final Component comp) {
 		comp.addKeyListener(this);
-		if(comp instanceof Container){
-			Container c = (Container)comp;
-			Component[] children = c.getComponents();
-			for(int i = 0; i < children.length; i++){
-				Component child = children[i];
+		if (comp instanceof Container) {
+			for (final Component child : ((Container) comp).getComponents()) {
 				addAccelerator(child);
 			}
 		}
 	}
 
-	public void componentRemoved(ContainerEvent arg0) {
+	@Override
+	public void componentRemoved(final ContainerEvent arg0) {
 		removeAccelerator(arg0.getChild());
 	}
 	
-	private void removeAccelerator(Component comp){
-		//System.out.println("removed key listener to " + comp);
+	private void removeAccelerator(final Component comp) {
 		comp.removeKeyListener(this);
-		if(comp instanceof Container){
-			Container c = (Container)comp;
-			Component[] children = c.getComponents();
-			for(int i = 0; i < children.length; i++){
-				Component child = children[i];
+		if (comp instanceof Container) {
+			for (final Component child : ((Container) comp).getComponents()) {
 				removeAccelerator(child);
 			}
 		}
 	}
 	
-	public void setFocusIndex(int index){
+	public void setFocusIndex(final int index) {
 		MatchResultReviewPanel mrrp = review_panels.get(focus_index);
 		mrrp.showBorder(false);
 		
@@ -74,15 +68,14 @@ public class MatchResultReviewKeyboardAccelerator implements KeyListener, Contai
 		mrrp.showBorder(true);
 	}
 	
-	public int getPanelIndex(MatchResultReviewPanel mrrp){
-		int ret = review_panels.indexOf(mrrp);
-		return ret;
+	public int getPanelIndex(final MatchResultReviewPanel mrrp) {
+		return review_panels.indexOf(mrrp);
 	}
 	
-	public void nextFocus(){
+	public void nextFocus() {
 		MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-		if(focus_index == review_panels.size() - 1){
-			if(!pager.isOnLastPage()){
+		if (focus_index == review_panels.size() - 1) {
+			if (!pager.isOnLastPage()) {
 				mrrp.showBorder(false);
 				pager.updateView(pager.getViewIndex() + MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
 				focus_index = 0;
@@ -97,10 +90,10 @@ public class MatchResultReviewKeyboardAccelerator implements KeyListener, Contai
 		}
 	}
 	
-	public void previousFocus(){
+	public void previousFocus() {
 		MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-		if(focus_index == 0){
-			if(!pager.isOnFirstPage()){
+		if (focus_index == 0) {
+			if (!pager.isOnFirstPage()) {
 				mrrp.showBorder(false);
 				pager.updateView(pager.getViewIndex() - MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
 				focus_index = review_panels.size() - 1;
@@ -114,49 +107,53 @@ public class MatchResultReviewKeyboardAccelerator implements KeyListener, Contai
 		}
 	}
 
-	public void keyPressed(KeyEvent e) {
-		if(!(e.getSource() instanceof JTextField)){
-			if(e.getKeyCode() == KeyEvent.VK_M){
-				MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-				mrrp.setAsMatch();
-			} else if(e.getKeyCode() == KeyEvent.VK_N){
-				MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-				mrrp.setAsNonMatch();
-			} else if(e.getKeyCode() == KeyEvent.VK_R){
-				MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-				mrrp.setAsNotReviewed();
-			} else if(e.getKeyCode() == KeyEvent.VK_PAGE_UP){
-				pager.updateView(pager.getViewIndex() - MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
-			} else if(e.getKeyCode() == KeyEvent.VK_PAGE_DOWN){
-				pager.updateView(pager.getViewIndex() + MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
-			} else if(e.getKeyCode() == KeyEvent.VK_SPACE){
-				nextFocus();
-			} else if(e.getKeyCode() == KeyEvent.VK_ENTER){
-				nextFocus();
-			} else if(e.getKeyCode() == KeyEvent.VK_DOWN){
-				nextFocus();
-			} else if(e.getKeyCode() == KeyEvent.VK_UP){
+	@Override
+	public void keyPressed(final KeyEvent e) {
+		if (e.getSource() instanceof JTextField) {
+			return;
+		}
+		final int keyCode = e.getKeyCode();
+		if (keyCode == KeyEvent.VK_M) {
+			review_panels.get(focus_index).setAsMatch();
+		} else if (keyCode == KeyEvent.VK_N) {
+			review_panels.get(focus_index).setAsNonMatch();
+		} else if (keyCode == KeyEvent.VK_R) {
+			review_panels.get(focus_index).setAsNotReviewed();
+		} else if (keyCode == KeyEvent.VK_PAGE_UP) {
+			pager.updateView(pager.getViewIndex() - MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
+		} else if (keyCode == KeyEvent.VK_PAGE_DOWN) {
+			pager.updateView(pager.getViewIndex() + MatchResultReviewPagerPanel.VIEWS_PER_PAGE);
+		} else if (keyCode == KeyEvent.VK_SPACE) {
+			nextFocus();
+			e.consume();
+		} else if (keyCode == KeyEvent.VK_TAB) {
+			if (e.isShiftDown()) {
 				previousFocus();
 			} else {
-				try{
-					int new_val =Integer.parseInt(Character.toString(e.getKeyChar()));
-					MatchResultReviewPanel mrrp = review_panels.get(focus_index);
-					mrrp.setGUICertainty(new_val);
-				}
-				catch(NumberFormatException nfe){
-					
-				}
+				nextFocus();
 			}
-			
-			
+			e.consume();
+		} else if (keyCode == KeyEvent.VK_ENTER) {
+			nextFocus();
+		} else if (keyCode == KeyEvent.VK_DOWN) {
+			nextFocus();
+			e.consume();
+		} else if (keyCode == KeyEvent.VK_UP) {
+			previousFocus();
+			e.consume();
+		} else {
+			try {
+				review_panels.get(focus_index).setGUICertainty(Integer.parseInt(Character.toString(e.getKeyChar())));
+			} catch (final NumberFormatException nfe) {
+			}
 		}
 	}
 
-	public void keyReleased(KeyEvent e) {
-		
+	@Override
+	public void keyReleased(final KeyEvent e) {
 	}
 
-	public void keyTyped(KeyEvent e) {
-		
+	@Override
+	public void keyTyped(final KeyEvent e) {
 	}
 }
